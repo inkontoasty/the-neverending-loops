b1d6a3ae41048febc4ed29a9fedc5ce6620b5df5
The Myth <dagamudith@gmail.com>
2023-09-07 18:55:58 +0530

    FIXED IT POG MOMENT

--- src/gui/main_gui.py
+++ src/gui/main_gui.py
@@ -5,7 +5,7 @@ from backend.typingcolors import TypingColors
 from backend.typingcolors_utils import typingcolors_load
 from gui.modules import ImageLabel
 from gui.win_steganography import SteganographyWin
-# from gui.win_typingcolors import TypingColorsWin
+from gui.win_typingcolors import TypingColorsWin
 
 WIN_W, WIN_H = (800, 600)
 POP_W, POP_H = (400, 300)
@@ -175,7 +175,7 @@ class GUI(Tk):
         if mode == 0:
             self.typingColors = TypingColors()
             self.typingColors.set_encryption(key)
-            self.callback(TypingColorsWin, [self.main])
+            self.callback(lambda: TypingColorsWin(self, self.typingColors), [self.main])
             # self.typingColorsWin = TypingColorsWin(self.typingColors)
         else:
             self.steganographyWin = SteganographyWin()
@@ -197,153 +197,3 @@ class GUI(Tk):
             self.typingColorsWin = TypingColorsWin(self.typingColors)
             self.typingColorsWin.text.delete(1.0, "end")
             self.typingColorsWin.text.insert("end", decoded_text)
-
-# win_typingcolors.py
-
-
-DARK_GRAY, GRAY = "#222831", "#393E46"
-AQUA, WHITE = "#00ADB5", "#EEEEEE"
-RED, GREEN = "#cd0000", "#1BAA4A"
-BRIGHT_RED = "#ff0000"
-
-
-class TypingColorsWin(GUI):
-    """Window for typingcolors"""
-
-    def __init__(self):
-        """Creates the layout"""
-        # super().__init__()
-        self.create_menu_bar()
-        self.typingColors = TypingColors()  # the main backend
-        self.file = None  # open files
-        self.title("New File - Typing Colors")
-        # split layout
-        self.text = Text(
-            self,
-            width=30,
-            height=15,
-            bg=DARK_GRAY,
-            fg="white",
-            font=("Consolas", 14),
-        )
-        self.canvas = Label(self, image=self.typingColors.img_scaled(), bg=DARK_GRAY)
-        self.info = StringVar()
-        self.info.set("0 characters   |   8px x 9px")
-        self.text.grid(row=0, column=0, sticky="nsew", rowspan=2)
-        self.canvas.grid(row=0, column=1, sticky="ne")
-        Label(self, textvariable=self.info, bg=DARK_GRAY, fg="white").grid(
-            row=1, column=1, sticky="e"
-        )
-        # start the loop
-        self._typingcolors_update("")
-
-    def _typingcolors_update(self, prev_txt):
-        """Update loop for typingcolours"""
-        txt = self.text.get("1.0", "end")
-        if txt != prev_txt:  # only update if text changed
-            self.typingColors.update(txt)
-            img = self.typingColors.img_scaled()
-            self.canvas.configure(image=img)
-            self.canvas.image = img
-            self.info.set(
-                f"{len(txt) - 1} characters   |   {self.typingColors.width}px x {self.typingColors.height}px"
-            )
-        self.after(50, lambda: self._typingcolors_update(txt))
-
-    def new(self):
-        """Resets the canvas and text"""
-        self.file = None
-        self.title("New File - Typing Colors")
-        self.text.delete(1.0, "end")
-        self.typingColors.update("")
-
-    def open(self):
-        """Opens and loads a selected text file"""
-        filename = fd.askopenfilename(title="Open", filetypes=[("All", "*.*")])
-        if filename:
-            content = open(filename, "r").read()
-            self.file = filename
-            self.title(f"{filename} - Typing Colors")
-            self.text.delete(1.0, "end")
-            self.text.insert("end", content)
-            self.typingColors.update(content)
-
-    def save(self):
-        """Saves the current text"""
-        if not self.file:  # no file to save to yet
-            self.saveas()  # open a file selector then save
-        else:
-            open(self.file, "w").write(self.text.get(1.0, "end"))
-
-    def saveas(self):
-        """Opens a file dialog and saves text to selected file"""
-        filename = fd.asksaveasfilename(title="Save As", filetypes=[("All", "*.*")])
-        if filename:
-            self.file = filename
-            self.title(f"{filename} - Typing Colors")
-            open(self.file, "w").write(self.text.get(1.0, "end"))
-
-    def export(self):
-        """Exports the canvas to a PNG"""
-        filename = fd.asksaveasfilename(title="Export As", filetypes=[("PNG", "*.png")])
-        if filename:
-            self.typingColors.canvas.save(filename, format="PNG")
-
-    def create_menu_bar(self):
-        """Packs the menu bar for the application"""
-        layouts = {
-            "File": {
-                "New": {
-                    "command": self.new,
-                    "image": "assets\\imgs\\new.png",
-                    "accelerator": "Ctrl+N",
-                },
-                "Open": {
-                    "command": self.open,
-                    "image": "assets\\imgs\\save.png",
-                    "accelerator": "Ctrl+O",
-                },
-                "Save": {
-                    "command": self.save,
-                    "image": "assets\\imgs\\save.png",
-                    "accelerator": "Ctrl+S",
-                },
-                "Save As": {
-                    "command": self.saveas,
-                    "image": "assets\\imgs\\save.png",
-                    "accelerator": "Ctrl+Shift+S",
-                },
-                "Export": {
-                    "command": self.export,
-                    "image": "assets\\imgs\\save.png",
-                    "accelerator": "Ctrl+I",
-                },
-                "---": "",
-                "Exit": {
-                    "command": self.destroy,
-                    "image": "assets\\imgs\\exit.png",
-                    "accelerator": "Alt+F4",
-                },
-            }
-        }
-
-        # Main Menu Bar
-        menubar = Menu(self, tearoff=0, font=("Consolas", 12))
-
-        # Add to menu bars
-        for label, layout in layouts.items():
-            menu = Menu(self, tearoff=0, cursor="hand1", font=("Consolas", 10))
-            menubar.add_cascade(label=label, menu=menu)
-            for name, data in layout.items():
-                if "-" in name:
-                    menu.add_separator()
-                else:
-                    menu.add_command(
-                        label=name,
-                        compound="left",
-                        command=data["command"],
-                        accelerator=data["accelerator"],
-                        activeforeground=WHITE,
-                        activebackground=GRAY,
-                    )
-        self.configure(background=DARK_GRAY, menu=menubar)
